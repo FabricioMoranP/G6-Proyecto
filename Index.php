308e8023b1e99848de118eeef1d4727a0ded981a
Fabricio Moran <90921548+FabricioMoranP@users.noreply.github.com>
2023-08-28 14:56:08 -0500

    update

---
+++ Index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="imagenes/capoo.ico">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap" rel="stylesheet">
+    <title>CR7</title>
+</head>
+
+<body>
+    <div class="header-navegador">
+        <header>    
+            <nav class="nav">
+                <div class="logo">
+                    <a href="Index.html"><img class="img-logo" src="imagenes/empresalogo.jpg" alt="logo de la marca"></a>
+                </div>
+                <a class="nav-a" href="Index.html">Inicio</a>
+                <a class="nav-a" href="Nosotros.html">Nosotros</a>
+                <a class="nav-a" href="Contactenos.html">Contactenos</a>
+                <a class="nav-a" href="crudclientes.php">Clientes (CRUD)</a>
+                <div class="nav-iniciarsesion">
+                    <a class="nav-a" href="IniciarSesion.html">Iniciar Sesion</a>
+                </div>
+            </nav>
+        </header>
+    </div>
+    <div class="contaner">
+        <div class="cuerpo-article">
+            <h1 class="article-subtitulo-h1">Cristiano Ronaldo ¿Quién es?</h1>
+                <p>
+                    Cristiano Ronaldo dos Santos Aveiro (Funchal, Madeira, 5 de febrero de 1985), conocido como Cristiano
+                    Ronaldo, es un futbolista portugués. Juega como extremo izquierdo o delantero y su equipo actual es el
+                    Al-Nassr F. C. de la Liga Profesional Saudí. Es internacional absoluto con la selección de Portugal, de
+                    la cual es capitán, máximo goleador histórico y jugador con más presencias con 200 partidos, logro
+                    alcanzado en las eliminatorias para la Eurocopa 2024, reconocido por el Libro Guinness de los récords​.
+                </p>
+                <p>
+                    Considerado con frecuencia el mejor y más completo futbolista, ​así como el mayor goleador del mundo,
+                    ​además de uno de los mejores de todos los tiempos; ​ es uno de los futbolistas más laureados de la
+                    historia, habiendo ganado, entre otras distinciones, cinco veces el Balón de Oro, cinco premios de la
+                    FIFA
+                    al mejor jugador del mundo y cuatro Botas de Oro. En 2020, tuvo el honor de ser elegido el mejor Jugador
+                    del
+                    Siglo XXI en la gala de los Globe Soccer Awards, convirtiéndose en el primer futbolista europeo y el
+                    primer
+                    portugués en recibirlo, además de ser incluido en el Dream Team del Balón de Oro.
+                </p>
+                <img class="article-img-cristanobalondeoro" src="imagenes/cristanobalondeoro.jpeg" alt="Cristiano balon de oro">
+
+                <h1 class="article-subtitulo-h1">El máximo goleador de la historia del Real Madrid</h1>
+                <p><b>Posición en el campo: </b>Delantero</p>
+                <p><b>Partidos Jugados: </b>438 oficiales</p>
+                <p><b>Goles marcados: </b>451</p>
+                <p><b>Internacional con Portugal: </b>154 veces</p>
+                <p>
+                    Cristiano Ronaldo forma parte de la leyenda del Real Madrid y siempre será recordado como uno de los
+                    grandes símbolos de su historia. Fue presentado en el Santiago Bernabéu el 6 de julio de 2009,
+                    acompañado por Eusebio y Alfredo Di Stéfano, y desde entonces no paró de marcar goles: 451 en 438
+                    partidos oficiales con el Real Madrid (más de uno por encuentro). Además vio puerta en todas las
+                    competiciones: 312 en la Liga, 105 en la Champions, 22 en la Copa del Rey, 6 en el Mundial de Clubes, 4
+                    en la Supercopa de España y 2 en la Supercopa de Europa.
+                </p>
+                <p>
+                    Nadie en la historia del club ha marcado tantos goles como el delantero portugués, cuyo palmarés en el
+                    Real Madrid es impresionante: 4 Champions League, 3 Mundiales de Clubes, 3 Supercopas de Europa, 2
+                    Ligas, 2 Copas del Rey y 2 Supercopas de España. A ellos suma 4 Balones de Oro, 3 Botas de Oro, 2
+                    Premios The Best, 3 Premios al Mejor Jugador de la UEFA y 3 Trofeos Pichichi.
+                </p>
+                <h3 class="article-subtitulo-h3">Trayectoria de récords</h3>
+                <p>
+                    A lo largo de sus nueve temporadas como madridista, Cristiano dejó varios récords impresionantes: máximo
+                    goleador de la historia del club, máximo goleador de la historia de la Copa de Europa (marcó 105 tantos
+                    en Champions con nuestro equipo); máximo goleador madridista en la Liga (312); más partidos de tres o
+                    más goles en la historia de la Liga (34); y más tantos en una temporada con el Real Madrid (61).
+                </p>
+                <p>
+                    Batió también el récord de goles en una edición de la Champions (17) y fue pichichi en 6 ediciones de
+                    esta competición. El presidente Florentino Pérez le calificó como “el heredero de Alfredo Di Stéfano”. Y
+                    como Don Alfredo, dejó una huella imbo  rrable en la historia del mejor club del mundo.
+                </p>
+                <img class="article-img-cristianorealmadrid" src="imagenes/cristianorealmadrid.jpg" alt="Cristiano 5 Champions"><br>
+                <h1 class="article-subtitulo-h1">El máximo goleador de la historia del Real Madrid</h1>
+        </div>
+        <div class="contenido-extra">
+                <h2 class="contenidoextra-h2">Otros Futbolistas</h2>
+                <div class="contenidoextra-enlaces">
+                    <a class="ce-a-jugadores" href="Index.html">Cristiano Ronaldo</a>
+                    <a class="ce-a-jugadores" href="">Leonel Messi</a>
+                    <a class="ce-a-jugadores" href="">Luka Modric</a>
+                    <a class="ce-a-jugadores" href="">Kilian Mbappe</a>
+                </div>
+        </div>
+    </div>
+    <div>
+        <footer class="footer">
+            <h4>Copyright - Todos los derechos reservados</h4>
+            <p>Siguenos en nuestras redes sociales</p>
+            <div class="footer-redes">
+                <a href="https://facebook.com"><img class="footer-img" src="imagenes/facebook.png" alt=""></a>
+                <a href="https://Twitter.com"><img class="footer-img" src="imagenes/gorjeo.png" alt=""></a>
+                <a href="https://Instagram.com"><img class="footer-img" src="imagenes/instagram.png" alt=""></a>
+            </div>
+        </footer>
+    </div>
+
+
+</body>
+
+</html>
