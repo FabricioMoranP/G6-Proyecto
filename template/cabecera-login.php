cd035d4b467718214fbf826adafa6e15e1ad2de8
Fabricio <fabriciofto@gmail.com>
2023-08-31 01:12:55 -0500

    update

--- template/cabecera-login.php
+++ template/cabecera-login.php
@@ -20,7 +20,7 @@
                 </div>
                 <a class="nav-a" href="Index.php">Inicio</a>
                 <a class="nav-a" href="Nosotros.php">Nosotros</a>
-                <a class="nav-a" href="Contactenos.php">Contactenos</a>
+                <!-- <a class="nav-a" href="Contactenos.php">Contactenos</a> -->
                 <a class="nav-a" href="clientes.php">Clientes (CRUD)</a>
                 <div class="nav-iniciarsesion">
                     <a class="nav-a" href="iniciarsesion.php">Iniciar Sesion</a>
