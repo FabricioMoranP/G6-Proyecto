a84c442ac89b5e580781ca2143074b52436c3e66
Fabricio <fabriciofto@gmail.com>
2023-09-01 20:40:53 -0500

    update

--- Contactenos.php
+++ Contactenos.php
@@ -1,4 +1,17 @@
-<?php include("template/cabecera.php")?>
+<?php include("template/cabecera.php");
+
+$accion=(isset($_POST['accion']))?$_POST['accion']:"";
+
+switch($accion){
+    case "Enviar":
+        
+    header("Location: Contactenos.php");
+    break;
+}
+if($accion){
+    $mensaje="Enviado con exito";
+}
+?>
 <!-- <!doctype html>
 <html lang="es">    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -20,7 +33,7 @@
                 <!-- Aquí pon las col-x necesarias, comienza tu contenido, etcétera -->
                 <div class="contaner-cont">
                     <h1 class="h1-cont">Gracias por contactarme</h1>
-                    <form class="form-cont" action="https://formsubmit.co/fabriciofto@email.com" method="POST">
+                    <form class="form-cont" method="POST">
                         <div class="form-group">
                             <label class="label-form" for="nombre">Nombre:</label><br>
                             <input name="nombre" required type="text" id="nombre"
@@ -36,9 +49,14 @@
                             <textarea required  class="form-control" class="form-txtarea" name="mensaje" id="mensaje"></textarea>
                         </div>
                         <div class="form-group">
-                            <button class="btn-cont" type="submit" name="enviar">Enviar</button>
+                            <button class="btn-cont" type="submit" name="accion" value="Enviar">Enviar</button>
                         </div>
                     </form>
+                    <?php if (!empty($mensaje)) { ?>
+                        <strong>
+                            <?php echo $mensaje; ?>
+                        </strong><br>
+                    <?php } ?>
                 </div>
-            </section>
+        </section>
 <?php include("template/pie.php")?>
